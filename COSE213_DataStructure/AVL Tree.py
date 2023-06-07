977843b0549dbdfc6b627ee97c12bf08e99db584
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-06-07 18:38:42 +0900

    230607 DataStructure AVL Tree prac

---
+++ COSE213_DataStructure/AVL Tree.py
@@ -0,0 +1,10 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.height = 0  
+
+a=Node(52)
+print(a.height)     
